a551f0f1862c7b47f71f43f8a16eac6ab66a3245
sajan25805 <sajanrajmainali99@gmail.com>
2023-09-11 00:18:06 +0545

    mui_configuration/theme Done git push  mui_configuration/theme Done git branch

--- Finance_Client/src/App.tsx
+++ Finance_Client/src/App.tsx
@@ -1,9 +1,18 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "../theme"
 
 
 function App() {  
+  const theme = useMemo(()=>createTheme(themeSettings),[])
+
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <ThemeProvider theme = { theme }>
+        <CssBaseline/>  {/** Kickstart an elegant, consistent, and simple baseline to build upon.*/}
+          Sajan Raj
+      </ThemeProvider>
     </div>
   );
 }
