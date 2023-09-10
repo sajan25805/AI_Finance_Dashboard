a842a351be7d7660df6b867ca45d2643a9dfb6fe
sajan25805 <sajanrajmainali99@gmail.com>
2023-09-10 23:19:37 +0545

    eslint configuration and src path to @ setup Done git branch

--- Finance_Client/vite.config.ts
+++ Finance_Client/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";   // To set the path of the imports. 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
+  }
 })
